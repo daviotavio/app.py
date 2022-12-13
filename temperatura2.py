fa4aff290404fdd5dd7cf2e4bddcbee795df4222
daviotavio <daviotavioribeiro@gmail.com>
2022-12-13 13:02:57 -0300

    novo exercício de temperaturas

---
+++ temperatura2.py
@@ -0,0 +1,6 @@
+print('Calculadora de graus que transfoma graus celcius em fahrenheit')
+print('Forneça a temperatura em graus celcius: ')
+graus_c = float(input())
+graus_f = (graus_c * 1.8) + 32
+
+print('A temperatura {0} graus em Celcius é igual a {1} em graus Fahrenheit'.format(graus_c, graus_f))
